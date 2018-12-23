63b0ef3d03d872d41bdf7f1ee527eeac30e103c7
raytency <raytency@gmail.com>
2018-12-23 19:25:13 +0900

    Vueインスタンス作成時にエラー出るの修正

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -8,12 +8,14 @@ if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
-  components: { App },
-  template: '<App/>'
-}).$mount('#app')
-
-new Vue({
-  components: { Screenshot },
-  template: '<Screenshot/>'
-}).$mount('#screenshot')
+if (location.href.match(/screenshot/)) {
+  new Vue({
+    components: { Screenshot },
+    template: '<Screenshot/>'
+  }).$mount('#screenshot')
+} else {
+  new Vue({
+    components: { App },
+    template: '<App/>'
+  }).$mount('#app')
+}

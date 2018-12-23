77f8afa5dc870694ce2a607bdde99c49cd319c69
raytency <raytency@gmail.com>
2018-12-23 17:20:26 +0900

    vue-electronを新しくした

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
-import axios from 'axios'
+
 import App from './App'
 import Screenshot from './Screenshot'
 import 'bulma/css/bulma.css'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
